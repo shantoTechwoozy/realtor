f429902b470279885df4bb330a816196c4b8b396
shantoTechwoozy <shanto.techwoozy@gmail.com>
2024-07-04 17:26:20 +0600

    contact banner changed

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,20 +1,25 @@
 const Contact = () => {
   return (
-    <section id="contact" className="relative py-20  md:py-[120px]">
+    <section id="contact" className="relative py-20 md:py-[120px]">
       <div className="absolute left-0 top-0 -z-[1] h-full w-full dark:bg-dark"></div>
-      <div className="absolute left-0 top-0 -z-[1] h-1/2 w-full bg-[#E9F9FF] dark:bg-dark-700 lg:h-[45%] xl:h-1/2 bg-[url('/images/action/contact.jpg')] bg-cover bg-center bg-no-repeat"></div>
+      <div className="relative overflow-hidden bg-[url('/images/action/contact.jpg')] bg-cover bg-center bg-no-repeat pt-[100px] md:pt-[130px] lg:pt-[150px]">
+        <div className="absolute inset-0 bg-black opacity-30 blur-lg"></div>
+        <div className="container relative z-10">
+          <div className="ud-contact-title mb-12 lg:mb-[150px] max-w-[600px]">
+            <span className="block text-base font-medium text-white dark:text-white">
+              CONTACT US
+            </span>
+            <h2 className="text-[35px] md:text-[50px] font-semibold leading-tight text-white dark:text-white">
+              Let&apos;s talk about your problem.
+            </h2>
+          </div>
+        </div>
+      </div>
       <div className="container px-4">
         <div className="-mx-4 flex flex-wrap items-center">
-          <div className="w-full px-4 mt-10  lg:w-7/12 xl:w-8/12">
-            <div className="ud-contact-content-wrapper ">
-              <div className="ud-contact-title mb-12 lg:mb-[150px]">
-                <span className="mb-6 block text-base font-medium text-white dark:text-white">
-                  CONTACT US
-                </span>
-                <h2 className="max-w-[260px] text-[35px] font-semibold leading-[1.14] text-white dark:text-white">
-                  Let&apos;s talk about your problem.
-                </h2>
-              </div>
+          <div className="mt-10 w-full px-4 lg:w-7/12 xl:w-8/12">
+            <div className="ud-contact-content-wrapper">
+             
               <div className="mb-12 flex flex-wrap justify-between lg:mb-0">
                 <div className="mb-8 flex w-[330px] max-w-full">
                   <div className="mr-6 text-[32px] text-primary">
@@ -70,16 +75,50 @@ const Contact = () => {
                   </div>
                   <div>
                     <h3 className="mb-[18px] text-lg font-semibold text-dark dark:text-white">
-                      Contact Details
+                      Our Location
                     </h3>
                     <p className="text-base text-body-color dark:text-dark-6">
-                      Shohidul Islam (Real Estate Agent | Realtor)
+                      37600 Central Ct#264, Newark, CA 94569
                     </p>
+                  </div>
+                </div>
+                <div className="mb-8 flex w-[330px] max-w-full">
+                  <div className="mr-6 text-[32px] text-primary">
+                    <svg
+                      width="34"
+                      height="25"
+                      viewBox="0 0 34 25"
+                      className="fill-current"
+                    >
+                      <path d="M30.5156 0.960938H3.17188C1.42188 0.960938 0 2.38281 0 4.13281V20.9219C0 22.6719 1.42188 24.0938 3.17188 24.0938H30.5156C32.2656 24.0938 33.6875 22.6719 33.6875 20.9219V4.13281C33.6875 2.38281 32.2656 0.960938 30.5156 0.960938ZM30.5156 2.875C30.7891 2.875 31.0078 2.92969 31.2266 3.09375L17.6094 11.3516C17.1172 11.625 16.5703 11.625 16.0781 11.3516L2.46094 3.09375C2.67969 2.98438 2.89844 2.875 3.17188 2.875H30.5156ZM30.5156 22.125H3.17188C2.51562 22.125 1.91406 21.5781 1.91406 20.8672V5.00781L15.0391 12.9922C15.5859 13.3203 16.1875 13.4844 16.7891 13.4844C17.3906 13.4844 17.9922 13.3203 18.5391 12.9922L31.6641 5.00781V20.8672C31.7734 21.5781 31.1719 22.125 30.5156 22.125Z" />
+                    </svg>
+                  </div>
+                  <div>
+                    <h3 className="mb-[18px] text-lg font-semibold text-dark dark:text-white">
+                      How Can We Help?
+                    </h3>
                     <p className="text-base text-body-color dark:text-dark-6">
-                      DRE # 02160189
+                      shohid.realty@gmail.com
                     </p>
+                  </div>
+                </div>
+                <div className="mb-8 flex w-[330px] max-w-full">
+                  <div className="mr-6 text-[32px] text-primary">
+                    <svg
+                      width="34"
+                      height="25"
+                      viewBox="0 0 34 25"
+                      className="fill-current"
+                    >
+                      <path d="M30.5156 0.960938H3.17188C1.42188 0.960938 0 2.38281 0 4.13281V20.9219C0 22.6719 1.42188 24.0938 3.17188 24.0938H30.5156C32.2656 24.0938 33.6875 22.6719 33.6875 20.9219V4.13281C33.6875 2.38281 32.2656 0.960938 30.5156 0.960938ZM30.5156 2.875C30.7891 2.875 31.0078 2.92969 31.2266 3.09375L17.6094 11.3516C17.1172 11.625 16.5703 11.625 16.0781 11.3516L2.46094 3.09375C2.67969 2.98438 2.89844 2.875 3.17188 2.875H30.5156ZM30.5156 22.125H3.17188C2.51562 22.125 1.91406 21.5781 1.91406 20.8672V5.00781L15.0391 12.9922C15.5859 13.3203 16.1875 13.4844 16.7891 13.4844C17.3906 13.4844 17.9922 13.3203 18.5391 12.9922L31.6641 5.00781V20.8672C31.7734 21.5781 31.1719 22.125 30.5156 22.125Z" />
+                    </svg>
+                  </div>
+                  <div>
+                    <h3 className="mb-[18px] text-lg font-semibold text-dark dark:text-white">
+                      How Can We Help?
+                    </h3>
                     <p className="text-base text-body-color dark:text-dark-6">
-                      408.649.1164
+                      shohid.realty@gmail.com
                     </p>
                   </div>
                 </div>
@@ -87,77 +126,74 @@ const Contact = () => {
             </div>
           </div>
           <div className="w-full px-4 lg:w-5/12 xl:w-4/12">
-            <div
-              className="wow fadeInUp rounded-lg bg-white px-8 py-10 shadow-testimonial dark:bg-dark-2 dark:shadow-none sm:px-10 sm:py-12 md:p-[60px] lg:p-10 lg:px-10 lg:py-12 2xl:p-[60px]"
-              data-wow-delay=".2s
-              "
-            >
-              <h3 className="mb-8 text-2xl font-semibold text-dark dark:text-white md:text-[28px] md:leading-[1.42]">
-                Send us a Message
-              </h3>
+            <div className="relative rounded-lg bg-white p-10 shadow-lg dark:bg-dark sm:p-12">
+              <div className="absolute inset-0 bg-black opacity-30 blur-lg"></div>
               <form>
-                <div className="mb-[22px]">
+                <div className="mb-6">
                   <label
-                    htmlFor="fullName"
-                    className="mb-4 block text-sm text-body-color dark:text-dark-6"
+                    htmlFor="name"
+                    className="block text-xs text-dark dark:text-white"
                   >
                     Full Name*
                   </label>
                   <input
                     type="text"
-                    name="fullName"
-                    placeholder="Adam Gelius"
-                    className="w-full border-0 border-b border-[#f1f1f1] bg-transparent pb-3 text-dark placeholder:text-body-color/60 focus:border-primary focus:outline-none dark:border-dark-3 dark:text-white"
+                    name="name"
+                    id="name"
+                    placeholder="Full Name"
+                    className="w-full rounded border-[#e9edf9] px-4 py-3 text-base text-body-color placeholder-body-color outline-none focus:border-primary focus-visible:shadow-none"
                   />
                 </div>
-                <div className="mb-[22px]">
+                <div className="mb-6">
                   <label
                     htmlFor="email"
-                    className="mb-4 block text-sm text-body-color dark:text-dark-6"
+                    className="block text-xs text-dark dark:text-white"
                   >
                     Email*
                   </label>
                   <input
                     type="email"
                     name="email"
+                    id="email"
                     placeholder="example@yourmail.com"
-                    className="w-full border-0 border-b border-[#f1f1f1] bg-transparent pb-3 text-dark placeholder:text-body-color/60 focus:border-primary focus:outline-none dark:border-dark-3 dark:text-white"
+                    className="w-full rounded border-[#e9edf9] px-4 py-3 text-base text-body-color placeholder-body-color outline-none focus:border-primary focus-visible:shadow-none"
                   />
                 </div>
-                <div className="mb-[22px]">
+                <div className="mb-6">
                   <label
                     htmlFor="phone"
-                    className="mb-4 block text-sm text-body-color dark:text-dark-6"
+                    className="block text-xs text-dark dark:text-white"
                   >
                     Phone*
                   </label>
                   <input
                     type="text"
                     name="phone"
-                    placeholder="+885 1254 5211 552"
-                    className="w-full border-0 border-b border-[#f1f1f1] bg-transparent pb-3 text-dark placeholder:text-body-color/60 focus:border-primary focus:outline-none dark:border-dark-3 dark:text-white"
+                    id="phone"
+                    placeholder="Phone Number"
+                    className="w-full rounded border-[#e9edf9] px-4 py-3 text-base text-body-color placeholder-body-color outline-none focus:border-primary focus-visible:shadow-none"
                   />
                 </div>
-                <div className="mb-[30px]">
+                <div className="mb-6">
                   <label
                     htmlFor="message"
-                    className="mb-4 block text-sm text-body-color dark:text-dark-6"
+                    className="block text-xs text-dark dark:text-white"
                   >
                     Message*
                   </label>
                   <textarea
                     name="message"
-                    rows={1}
-                    placeholder="type your message here"
-                    className="w-full resize-none border-0 border-b border-[#f1f1f1] bg-transparent pb-3 text-dark placeholder:text-body-color/60 focus:border-primary focus:outline-none dark:border-dark-3 dark:text-white"
+                    id="message"
+                    placeholder="Type your message here"
+                    className="w-full rounded border-[#e9edf9] px-4 py-3 text-base text-body-color placeholder-body-color outline-none focus:border-primary focus-visible:shadow-none"
                   ></textarea>
                 </div>
                 <div className="mb-0">
                   <button
                     type="submit"
-                    className="inline-flex items-center justify-center rounded-md bg-primary px-10 py-3 text-base font-medium text-white transition duration-300 ease-in-out hover:bg-primary/90"
+                    className="inline-flex items-center justify-center rounded bg-primary px-6 py-3 text-base font-medium text-white transition duration-300 ease-in-out hover:bg-opacity-90"
                   >
-                    Send
+                    Send Message
                   </button>
                 </div>
               </form>
