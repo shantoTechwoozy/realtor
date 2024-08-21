f103e1de5bff4c8a5ec29c7323d11bc578c2439e
shantoTechwoozy <shanto.techwoozy@gmail.com>
2024-08-21 15:54:36 +0600

    loader removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,43 +6,23 @@ import ScrollToTop from "@/components/ScrollToTop";
 import { ThemeProvider } from "next-themes";
 import "../styles/index.css";
 import "../styles/prism-vsc-dark-plus.css";
-import { useEffect, useState } from "react";
-import PreLoader from "@/components/Common/PreLoader";
 import { ClerkProvider } from '@clerk/nextjs';
 import CookieConsent from "@/components/Cookies/page";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    setTimeout(() => setLoading(false), 1000);
-  }, []);
-
+export default function RootLayout({ children }) {
   return (
     <html suppressHydrationWarning={true} className="!scroll-smooth" lang="en">
       <head />
       <body>
-        {loading ? (
-          <PreLoader />
-        ) : (
-          <ThemeProvider
-            attribute="class"
-            enableSystem={false}
-            defaultTheme="light"
-          >
-            <ClerkProvider>
-              <Header />
-              {children}
-              <Footer />
-              <ScrollToTop />
-              <CookieConsent /> {/* Include the CookieConsent component */}
-            </ClerkProvider>
-          </ThemeProvider>
-        )}
+        <ThemeProvider attribute="class" enableSystem={false} defaultTheme="light">
+          <ClerkProvider>
+            <Header />
+            {children}
+            <Footer />
+            <ScrollToTop />
+            <CookieConsent /> {/* Include the CookieConsent component */}
+          </ClerkProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
